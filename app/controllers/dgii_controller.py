e02d1583f5deba3e7160a62ee7b0eb92161bd557
JR-Camacho <josecamachoc0303@gmail.com>
2024-07-28 21:34:17 -0400

    Se actualiza el README.MD

--- app/controllers/dgii_controller.py
+++ app/controllers/dgii_controller.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 import os
+import json
 
 from app.services.dgii_service import RNC
 
@@ -14,7 +15,7 @@ def get_general():
 
 
 @router.get("/rnc")
-def get_all_rnc():
+def get_all_rnc(page: int = Query(default=1, ge=1), page_size: int = Query(default=10, ge=1)):
     data_route = os.path.join(
         os.path.dirname(__file__), '../../data/dgii/rnc/TMP/DGII_RNC.TXT')
 
@@ -23,9 +24,34 @@ def get_all_rnc():
         RNC.get_rnc_list()
     result = RNC.extract_data()
 
-    # Convertir el DataFrame a un arreglo de objetos JSON
-    json_result = result.to_json(orient='records')
+    # # Convertir el DataFrame a un arreglo de objetos JSON
+    # json_result = result.to_json(orient='records')
 
-    # Imprimir el resultado
-    # return json_result
-    return {"all rnc data": json_result}
+    # # Imprimir el resultado
+    # # return json_result
+    # return {"all rnc data": json_result}
+
+    # Calcular el total de páginas
+    total_rows = len(result)
+    total_pages = (total_rows + page_size - 1) // page_size
+
+    # Calcular el rango de filas para la página solicitada
+    start_row = (page - 1) * page_size
+    end_row = start_row + page_size
+
+    # Filtrar el DataFrame para la paginación
+    paginated_result = result.iloc[start_row:end_row]
+
+    # Convertir el DataFrame paginado a un arreglo de objetos JSON
+    json_result = paginated_result.to_json(orient='records')
+
+    # Convertir la cadena de texto JSON a un objeto Python
+    data_object = json.loads(json_result)
+
+    # Devolver el resultado junto con información de paginación
+    return {
+        "page": page,
+        "page_size": page_size,
+        "total_pages": total_pages,
+        "data": data_object
+    }
